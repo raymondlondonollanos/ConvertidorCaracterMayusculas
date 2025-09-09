98b8eb57d132c4cbb3ae00e7349e173a6a8ac56e
raymondlondonollanos <95382390+raymondlondonollanos@users.noreply.github.com>
2025-09-09 15:21:28 -0500

    Update OperacionesConChar.cpp

--- ConvertidorCaracterMayusculas/OperacionesConChar.cpp
+++ ConvertidorCaracterMayusculas/OperacionesConChar.cpp
@@ -5,14 +5,19 @@
 #include <algorithm>
 #include <cctype>
 
-//Los array fijos fijos siempre decaen a punteros por eso podemos utilizar [] o char* como notacion
+//Los array fijos fijos siempre decaen a punteros por eso podemos utilizar [] o char* 
+//como notacion
 void operacionesConChar::fn( const char* fr)
 {
-	//utilizamos size_t que es un tipo integer positivo, el cual optimiza el bucle, este itereara n veces hasta que encuente un terminador nulo
+	//utilizamos size_t que es un tipo integer positivo, el cual optimiza el bucle,
+	//este itereara n veces hasta que encuente un terminador nulo
 	for (std::size_t i{ 0 }; fr[i] != '\0'; ++i)
 	{
-		//cuando i = 0, fr[0], el cual tendra el valor de H, tolower recive valores unsigend char por eso es necesario realizar ese 
-		//conversion para evitar incovenientes. tolower devolver un valor integer tipo size_t el cual debera ser otra vez convertido con estatic cast para poder mostar en pantalla
+		//cuando i = 0, fr[0], el cual tendra el valor de H, tolower recive valores 
+		//unsigend char por eso es necesario realizar ese 
+		//conversion para evitar incovenientes. tolower devolver un valor integer 
+		//tipo size_t el cual debera ser otra vez convertido con estatic cast para 
+		//poder mostar en pantalla
 		std::cout << static_cast<char>(tolower(static_cast<unsigned char>(fr[i])));
 	}
 
@@ -20,8 +25,10 @@ void operacionesConChar::fn( const char* fr)
 
 void operacionesConChar::conversionModerna(std::string_view vista)
 {
-	//creamos una vista porque para utilizar transform necesitamos que el objeto sea dueno, aunque string_view no es dueño 
-	//cundo inicializamos  copia crea un objeto en binario lo cual nos permite realizar algunas modificaciones a esa vista
+	//creamos una vista porque para utilizar transform necesitamos que el objeto sea 
+	//dueno, aunque string_view no es dueÃ±o 
+	//cundo inicializamos  copia crea un objeto en binario lo cual nos permite 
+	//realizar algunas modificaciones a esa vista
 	std::string copia{ vista };
 
 	//std::transforma 
