dd230805b6e086ebd8fe73218884b1388765172c
Raymond londoño <raymond_5400@hotmail.com>
2025-09-09 14:53:13 -0500

    agregando nuevas funcionalidaes

--- ConvertidorCaracterMayusculas/main.cpp
+++ ConvertidorCaracterMayusculas/main.cpp
@@ -7,21 +7,28 @@
 
 int main()
 {
+    const char frase[] {"Hola amor"};
 
-	const char frase[] {"Hola amor"};
+    operacionesConChar::fn(frase);
 
-	operacionesConChar::fn(frase);
+    const char frase2[]{ " H1223olaWWW a???or" };
 
+    std::cout << std::endl;
 
-	const char frase2[]{ " H1223olaWWW a???or" };
+    operacionesConChar::fn(frase2);
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	operacionesConChar::fn(frase2);
+    operacionesConChar::conversionModerna(frase);
 
-	std::cout << std::endl;
+    char nuevaFrase[25]{};
 
-	operacionesConChar::conversionModerna(frase);
+    // solicitar al usuario que ingrese una frase para convertir todas en letras minusculas
+    std::cout << "\nIngresa una frase para convertir letras a minusculas: ";
+    // si utilizamos char para almacenar es importante definir para garantizar el terminador nulo
+    std::cin.getline(nuevaFrase, 25);
 
-	return 0;
+    operacionesConChar::conversionModerna(nuevaFrase);
+
+    return 0;
 }
